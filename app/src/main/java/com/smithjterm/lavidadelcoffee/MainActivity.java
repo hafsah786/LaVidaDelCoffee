dbd078625d9d15406918f549375bca9213c53d25
Demo User <demouser@MacBook-Air.local>
2018-01-12 14:46:01 -0500

    hafsah comment awesomeness

--- app/src/main/java/com/smithjterm/lavidadelcoffee/MainActivity.java
+++ app/src/main/java/com/smithjterm/lavidadelcoffee/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //changing something haha
+    // hafsah is typing this awesome comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
