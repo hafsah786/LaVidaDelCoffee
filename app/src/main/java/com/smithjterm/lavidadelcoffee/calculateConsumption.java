65689a4345131a4326264f8d59c1aa1275755b6f
hafsah786 <hhanif@smith.edu>
2018-01-18 10:23:15 -0500

    adding in get amount consumed form user input

---
+++ app/src/main/java/com/smithjterm/lavidadelcoffee/calculateConsumption.java
@@ -0,0 +1,8 @@
+package com.smithjterm.lavidadelcoffee;
+
+/**
+ * Created by demouser on 1/18/18.
+ */
+
+public class calculateConsumption {
+}
--- app/src/main/java/com/smithjterm/lavidadelcoffee/consumedActivity.java
+++ app/src/main/java/com/smithjterm/lavidadelcoffee/consumedActivity.java
@@ -1,7 +1,9 @@
 package com.smithjterm.lavidadelcoffee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class consumedActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class consumedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumed);
     }
+
+    public int getConsumed (){
+        TextView input = (TextView) findViewById(R.id.userinput_box);
+        String inputString = input.getText().toString();
+        return Integer.parseInt(inputString);
+    }
+
 }
