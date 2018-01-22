8aeeb19ec2abdcf367c0348ef01a7a1154724b7b
hafsah786 <hhanif@smith.edu>
2018-01-22 12:56:37 -0500

    changed ingredients content for latte/caramel macchiato

--- app/src/main/java/com/smithjterm/lavidadelcoffee/iceCaramelMacActivity.java
+++ app/src/main/java/com/smithjterm/lavidadelcoffee/iceCaramelMacActivity.java
@@ -1,7 +1,9 @@
 package com.smithjterm.lavidadelcoffee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class iceCaramelMacActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class iceCaramelMacActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ice_caramel_mac);
+    }
+
+    public void locationAct(View view) {
+        Intent i = new Intent(this, MacchiatoLocationActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/smithjterm/lavidadelcoffee/iceLatteMacActivity.java
+++ app/src/main/java/com/smithjterm/lavidadelcoffee/iceLatteMacActivity.java
@@ -1,7 +1,9 @@
 package com.smithjterm.lavidadelcoffee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class iceLatteMacActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class iceLatteMacActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ice_latte_mac);
+    }
+
+    public void locationAct(View view) {
+        Intent i = new Intent(this, MacchiatoLocationActivity.class);
+        startActivity(i);
     }
 }
